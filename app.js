64e91e5b22eb431ab2abb17943b96f92f5030626
Theo Breaux <theobreaux@gmail.com>
2023-02-07 17:20:55 -0800

    second commit

--- app.js
+++ app.js
@@ -14,10 +14,8 @@ function getList() {
     //get list of movies from local storage
     return JSON.parse(filmsStored);
 }
-
 //assign resulting array from getList invocation to variable
 let watchlistData = getList();
-
 //grab wishlist div
 const wishlist = document.getElementById("wishlist");
 //turn off display none, set to block
@@ -30,11 +28,36 @@ if (watchlistData.length > 0) {
         //assign classname to movieTitleDiv
         movieTitleDiv.className = "checkbox-container";
         //update innerHTML of div to reflect films already on wishlist
-        movieTitleDiv.innerHTML = ` <input id="movie-checkbox" type="checkbox" name="${element.movieName}">
+        movieTitleDiv.innerHTML = ` <input id="${element.movieName}" class="checkbox" type="checkbox">
                                     <label class="watchlist-checkbox" for="${element.movieName}">${element.movieName}</label>`;
         //append movieTitleDiv to wishlist
         wishlist.append(movieTitleDiv);
+
+        //grab savedFilms of watchlistData checkboxes
+        const savedFilms = document.querySelectorAll(".watchlist-checkbox");
+        //keep the checkbox checked 
+        const movieCheckboxes = document.getElementsByClassName("checkbox");
+
+        // loop through savedFilms checkboxes to change styling of watched films
+        for (let i = 0; i < savedFilms.length; i++) {
+            if (element.watched === true) {
+                //change styling of movie title after box is checked
+                savedFilms[i].style.textDecoration = "line-through black";
+                movieCheckboxes[i].checked = true;
+            }
+        }
     });
+
+    //grab checkboxDivs if they exist which include input and label w/ movie title
+    const movieCheckboxes = document.querySelectorAll(".checkbox-container");
+    //add click event listener to checkboxes using a loop, add remove film function
+    for (let i = 0; i < movieCheckboxes.length; i++) {
+        const movieLabel = movieCheckboxes[i].children[1];
+        movieCheckboxes[i].addEventListener("click", () => {
+            removeFilm(movieLabel);
+        });
+    }
+
 }
 
 //add submit eventlistener to form
@@ -56,7 +79,10 @@ movieForm.addEventListener("submit", async function (e) {
         const html = `<img class="movie-poster" src=${movieRequestPoster}>
                             <p class="title">${movieRequestTitle}</p>
                             <p class="release-year">${movieRequestYear}</p>
-                            <button id="wishlist-btn">Wishlist</button>`;
+                            <div class="tooltip">
+                                <button id="wishlist-btn">Wishlist</button>
+                                <span class="tooltiptext">Title is already in wishlist!</span>
+                            </div>`;
 
         //grab returned film div on page
         const returnedFilmDiv = document.querySelector(".returned-film");
@@ -80,67 +106,62 @@ movieForm.addEventListener("submit", async function (e) {
 
 //define function to create and add items to wishlist
 //create a function that will append a list item to an ul and save the names of the films
+
 const addToWishlist = (movieRequestTitle) => {
+    //disable watchlist button if title is already in watchlistData array
+    //styoe cursor to not allowed
     for (let i = 0; i < watchlistData.length; i++) {
-        const currMovieObj = watchlistData[i];
-        if (movieRequestTitle === currMovieObj.movieName) {
-            alert(`"${movieRequestTitle}" is already on your watchlist. Please search for another title.`);
+        if (movieRequestTitle === watchlistData[i].movieName) {
+            const addToWishlistBtn = document.querySelector("#wishlist-btn");
+            addToWishlistBtn.disabled = true;
+            addToWishlistBtn.style.cursor = "not-allowed";
+            const tooltip = document.querySelector(".tooltip");
+            tooltip.style.display = "inline-block";
             return;
         }
     }
+
     watchlistData.push({ movieName: movieRequestTitle, watched: false });
     //grab wishlist Div
     const wishlistDiv = document.querySelector(".wishlist-div");
     //change Div from display none to block so it now appears 
     wishlistDiv.style.display = "block";
-
-
     //create new div to hold input checkboxes
     const checkboxDiv = document.createElement("div");
     //give new div a classname 
     checkboxDiv.className = "checkbox-container";
     //update innerHTML of div to reflect new film added to wishlist
-    checkboxDiv.innerHTML = `       <input id="movie-checkbox" type="checkbox" name="${movieRequestTitle}">
+    checkboxDiv.innerHTML = `       <input id="${movieRequestTitle}" class="checkbox" type="checkbox">
                                     <label class="watchlist-checkbox" for="${movieRequestTitle}">${movieRequestTitle}</label>`;
     //append checkbox div to wishlist                                  
-    wishlistDiv.append(checkboxDiv)
+    wishlistDiv.append(checkboxDiv);
+
+    //grab label child of checkboxDiv
+    const movieLabel = checkboxDiv.children[1];
+    //add event listener to checkboxDiv
+    checkboxDiv.addEventListener("click", () => {
+        removeFilm(movieLabel);
+    });
+
     //set JS object values as JSON and store to local storage as watchlistData array
     localStorage.setItem("wishlist", JSON.stringify(watchlistData));
 }
 
-//grab checkboxDivs which include input and label w/ movie title
-const movieCheckboxes = document.querySelectorAll(".checkbox-container");
-//add click event listener to checkboxes using a loop, add remove film function
-for (let i = 0; i < movieCheckboxes.length; i++) {
-    const currChild = movieCheckboxes[i].children;
-    movieCheckboxes[i].addEventListener("click", () => {
-        removeFilm(currChild);
-    });
-}
-
-function removeFilm(currChild) {
-    //capture dom node for label tag
-    const movieLabel = currChild[1];
-    //change styling of movie title after box is checked
-    movieLabel.style.textDecoration = "line-through black";
-
+function removeFilm(movieLabel) {
     //loop through watchlistData to change watched property to true
     for (let i = 0; i < watchlistData.length; i++) {
         const currObj = watchlistData[i];
-        if (currObj.movieName === movieLabel.innerText) {
+        if (currObj.movieName === movieLabel.innerText && currObj.watched === true) {
             //changed watched property value to true
+            currObj.watched = false;
+            //change styling of movie title after box is checked
+            movieLabel.style.textDecoration = "none";
+        } else if (currObj.movieName === movieLabel.innerText && currObj.watched === false) {
             currObj.watched = true;
-            //declare variable to grab index of selection in array
-            const removeIndex = watchlistData.indexOf(currObj);
-            //remove film title from watchlistData array
-            watchlistData.splice(removeIndex, 1);
-            //update watchlistData array and set JS object values as JSON and store to local storage
-            localStorage.setItem("wishlist", JSON.stringify(watchlistData));
+            //change styling of movie title after box is checked
+            movieLabel.style.textDecoration = "line-through black";
         }
     }
+    //update watchlistData array and set JS object values as JSON and store to local storage
+    localStorage.setItem("wishlist", JSON.stringify(watchlistData));
 }
-
-// DONE - instead of using local storage to add films, addtowishlist function should take parameter to add to the wishlist. lines 50, 67, 69
-//DONE - LINE 67-84 - how to prevent in the wishlist there being the same title more than once conditional if statement solved this
-//DONE - checkboxes and strikethrough
-
